1f8d0fc38e4d3723e98ced2c5ccfe16bc4581e7b
Trillium <spiteless@gmail.com>
2020-08-07 11:38:04 -0700

    First versions of Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import './Header.css';
 // import {Link} from 'react-router-dom'
 
-export default class Header extends React.Component{
-    constructor(){
+export default class Header extends React.Component {
+    constructor() {
         super()
     }
-    render(){
-        return(
-            <div className='header'>
-                <h1>Header</h1>
+    render() {
+        return (
+            <div className='Header nav-container'>
+                <nav>
+                    <div className="logo-container">
+                        <div className="logo">LOGO</div>
+                    </div>
+                    <h1 className="logo-title">Shelfie</h1>
+                </nav>
             </div>
         )
     }
